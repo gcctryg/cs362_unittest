5c2cc1f98dd3a43b7e2675b0c21dd2443501ff93
Xuelu Feng <gcctryg@gmail.com>
2021-04-30 18:07:51 -0700

    unittest framework

--- calc.py
+++ calc.py
@@ -9,8 +9,3 @@ def multip(a,b):
 
 def div(a,b):
     return a / b
-
-
-    # myList = [summation, subtraction, multiplication, division]
-    # Sum = sum(myList)
-    # return Sum
--- test_calc.py
+++ test_calc.py
@@ -5,25 +5,31 @@ class testCaseAdd(unittest.TestCase):
     def test_add(self):
         #result = calc.add(10,5)
        # self.assertEqual(result, 15)
-        self.assertEqual(calc.add(10, 5), 15)
+        self.assertEqual(calc.add(10, 5), 14)
+
+    def test_type(self):
+        self.assertRaises(TypeError)
 
 class testCaseSubtra(unittest.TestCase):
     def test_add(self):
-        #result = calc.add(10,5)
-       # self.assertEqual(result, 15)
         self.assertEqual(calc.subtra(10, 5), 5)
+
+    def test_type(self):
+        self.assertRaises(TypeError)
 
 class testCaseMultip(unittest.TestCase):
     def test_add(self):
-        #result = calc.add(10,5)
-       # self.assertEqual(result, 15)
         self.assertEqual(calc.multip(10, 5), 50)
+
+    def test_type(self):
+        self.assertRaises(TypeError)
 
 class testCaseDiv(unittest.TestCase):
     def test_add(self):
-        #result = calc.add(10,5)
-       # self.assertEqual(result, 15)
         self.assertEqual(calc.div(10, 5), 2)
 
+    def test_type(self):
+        self.assertRaises(TypeError)
+        
 if __name__ == '__main__ ':
     unittest.main()
